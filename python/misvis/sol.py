32ec111d9421f10973d81623ea901baea75d5d6f
Lev Litvinov <misery7100@gmail.com>
2022-01-02 20:54:44 +0300

    sol plot module implemented (grid based)

--- python/misvis/sol.py
+++ python/misvis/sol.py
@@ -1,10 +1,13 @@
 import pandas as pd
-import numpy as np
 from .engine import Engine
+from .utils import configure_mpl
+import matplotlib.ticker as ticker
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.pyplot as plt
 
 # ------------------------- #
 
-class SolEngine:
+class SolEngine(Engine):
 
     # ------------------------- #
 
@@ -17,12 +20,12 @@ class SolEngine:
         ) -> None:
 
         super().__init__(
-            
-                loadmethod=pd.read_csv, 
-                fname=fname, 
-                **kwargs
-            
-            )
+
+            fname=fname,
+            loadmethod=pd.read_csv,  
+            **kwargs
+        
+        )
 
     # ------------------------- #
 
@@ -41,23 +44,153 @@ class ScattInd(SolEngine):
     def __init__(
             
             self, 
-            fname: str, 
+            fname: str,
             **kwargs
             
         ) -> None:
 
-        super().__init__(fname, **kwargs)
+        super().__init__(
+            
+            fname, 
+            comment='%', 
+            header=None,
+            **kwargs
+            
+        )
 
 # ------------------------- #
 
 class Field2D(SolEngine):
 
+    COLUMNS = {
+
+        0 : 'x',
+        1 : 'y',
+        2 : 'z'
+
+    }
+
+    TARGET_PLOT = {
+
+        'plot'      : True,
+        'radius'    : 8.9,
+        'color'     : 'white',
+        'alpha'     : 0.1
+
+    }
+
+    # ------------------------- #
+
     def __init__(
             
             self, 
-            fname: str, 
+            fname: str,
+            vars: tuple = ('normE',),
+            normal: str = 'y',
             **kwargs
             
         ) -> None:
 
-        super().__init__(fname, **kwargs)
+        super().__init__(
+                        
+            fname, 
+            comment='%', 
+            header=None,
+            **kwargs
+            
+        )
+
+        rename = {
+
+            **self.COLUMNS, 
+            **dict(
+                (3 + i, vars[i]) 
+                for i in range(len(vars))
+                )
+            
+        }
+        self.data.rename(columns=rename, inplace=True)
+        self.data.drop(normal, axis=1, inplace=True)
+
+        inplane = [x for x in self.COLUMNS.values() if x != normal]
+        self.grid_max = self.data[inplane].max().max()
+        self.grid_step = max(self.data[inplane[0]].diff()[1], self.data[inplane[1]].diff()[1])
+
+        length = self.data.shape[0]
+
+        transformed_data = dict()
+
+        for v in vars:
+
+            transformed_data[v] = (self.data[v]
+                    
+                    .to_numpy()
+                    .reshape(int(length ** 0.5), int(length ** 0.5))
+                    [::-1, :]
+                
+                )
+        
+        self.data = transformed_data
+        self.vars = vars
+    
+    # ------------------------- #
+
+    def plot_var(
+        
+            self, 
+            var: str = 'normE',
+            trim: int = 0,
+            font_scale: float = 2.0,
+            xtick: float = None,
+            ytick: float = None,
+            target: dict = dict(),
+            **kwargs
+        
+        ) -> tuple:
+
+        if var not in self.vars:
+
+            raise ValueError(f'{var} is not in the data')
+        
+        pltdata = self.data[var][trim:-trim, trim:-trim] if trim > 0 else self.data[var]
+        extval = self.grid_max - self.grid_step * trim
+        extent = [-extval, extval] * 2
+
+        configure_mpl(font_scale=font_scale)
+
+        fig, ax = plt.subplots(**kwargs)
+        field = ax.imshow(pltdata, cmap='hot', extent=extent)
+
+        divider = make_axes_locatable(ax)
+        cax = divider.append_axes("right", size="5%", pad=0.4)
+        plt.colorbar(field, cax=cax)
+
+        # circle2 = plt.Circle((5, 5), 0.5, color='b', fill=False)
+
+        target = {**self.TARGET_PLOT, **target}
+
+        if target.get('plot'):
+
+            circle = plt.Circle(
+                
+                    (0, 0), 
+                    target.get('radius'), 
+                    color=target.get('color'),
+                    alpha=target.get('alpha'), 
+                    fill=False
+                
+                )
+            ax.add_patch(circle)
+
+        ax.set_xlabel(r'$x$, $\rm{nm}$', labelpad=15)
+        ax.set_ylabel(r'$z$, $\rm{nm}$', labelpad=15)
+
+        if xtick: 
+            
+            ax.xaxis.set_major_locator(ticker.MultipleLocator(xtick))
+
+        if ytick: 
+            
+            ax.yaxis.set_major_locator(ticker.MultipleLocator(ytick))
+        
+        return fig, ax
