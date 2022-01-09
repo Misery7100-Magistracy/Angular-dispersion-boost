ab25d6f924770e5f90ed13efa3fe7d13424290ab
Lev Litvinov <misery7100@gmail.com>
2022-01-09 15:42:53 +0300

    iclo2022 template and so on

--- python/misvis/utils.py
+++ python/misvis/utils.py
@@ -1,6 +1,23 @@
-import matplotlib.pyplot as plt
 import matplotlib as mpl
 import seaborn as sns
+
+import matplotlib.pyplot as plt
+# from matplotlib.backends.backend_pgf import FigureCanvasPgf
+
+# mpl.use('pgf')
+
+# pgf_options = {
+#     "text.usetex": True,
+#     "pgf.texsystem": "xelatex",
+#     "pgf.rcfonts": True,
+#     "pgf.preamble": [
+#         r"\usepackage{amsmath,amssymb}",
+#         r"\usepackage{fontspec}",
+#         r"\usepackage{newtxtext,newtxmath}"
+#     ]
+#     }
+
+# mpl.rcParams.update(pgf_options)
 
 # ------------------------- #
 
@@ -26,8 +43,10 @@ RCDICT = {
 
 TEXRC = {
 
+
     "mathtext.fontset"  : "custom",
-    "font.weight"       : "light",
+    "font.family"       : "New Century Schoolbook",
+    #"font.weight"       : "bold",
     "font.serif"        : "New Century Schoolbook",
     "font.family"       : "New Century Schoolbook",
     "font.cursive"      : "New Century Schoolbook",
@@ -44,7 +63,13 @@ def configure_mpl(font_scale: float = 2.0):
     global TEXRC
 
     plt.rc('text', usetex=True)
-    plt.rc('text.latex', preamble=r"\usepackage{fouriernc}")
-
     mpl.rcParams.update(TEXRC)
+    #plt.rc('font', family='times')
+    plt.rc('text.latex', preamble=r"\usepackage{fouriernc}")
+    # plt.rc('text.latex', preamble=  r"\usepackage{amsmath, amssymb, amsfonts}"
+    #                                 r"\usepackage{textcomp}"
+    #                                 r"\usepackage{mathptmx}"
+    #                                 )
+
+    
     sns.set(font_scale=font_scale, style='white')
