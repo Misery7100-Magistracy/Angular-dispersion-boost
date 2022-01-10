83ec8fb01aa59f1e2ee15cfb0e0bba710142d485
Lev Litvinov <misery7100@gmail.com>
2022-01-10 19:46:51 +0300

    folder refactor (2); shorten ICLO2022

--- python/mesh.py
+++ python/mesh.py
@@ -39,6 +39,50 @@ class MeshGenerator:
             output = list(map(lambda x: list(x) + [0, radius, 0, m], coords))
         
         self.data = output
+
+    # ------------------------- #
+
+    @staticmethod
+    def rotate(
+
+            data,
+            a=0
+
+        ):
+
+        return data.dot(np.array([
+
+                [np.cos(a), 0, np.sin(a)],
+                [0, 1, 0],
+                [-np.sin(a), 0, np.cos(a)]
+                
+            ]))
+
+    # ------------------------- #
+
+    def project_2d(
+
+            self,
+            radius=20.,
+            gap=10.,
+            edge=10.,
+            mult=4,
+            angle=0
+
+        ):
+
+        angle *= (np.pi / 180)
+
+        rng = (2*radius + gap) * (edge - 1) / 2
+        coords1 = np.linspace(-rng, rng, edge)
+        coords2 = np.linspace(-rng*mult, rng*mult, edge*mult)
+        coords = np.array(list(map(list, product(coords2, coords1))))
+        coords = np.concatenate([coords, np.zeros((coords.shape[0], 1))], axis=1)
+        coords = coords[:, [0, 2, 1]]
+        coords = self.rotate(coords, angle)
+        output = list(map(lambda x: list(x) + [radius], coords))
+
+        return np.array(output)
     
     # ------------------------- #
 
