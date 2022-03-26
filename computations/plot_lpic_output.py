71c1d00ca7ec76ec39cf2629e5559b38be98d38c
Lev Litvinov <misery7100@gmail.com>
2022-03-26 22:57:06 +0300

    data files removed from lpic; gitkeep added

---
+++ computations/plot_lpic_output.py
@@ -0,0 +1,20 @@
+import argparse
+from processing import mis
+
+# ------------------------- #
+
+def preprocess_data(kind):
+
+    pass
+
+# ------------------------- #
+
+def save_result(fname):
+
+    pass
+
+# ------------------------- #
+
+if __name__ == "__main__":
+
+    pass
