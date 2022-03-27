e5b6a322a0a66ee67a26806b8dce5eb38f84194d
Lev Litvinov <misery7100@gmail.com>
2022-03-27 18:10:42 +0300

    mstm config edited; powershell script fixes

--- computations/matlab/run.ps1
+++ computations/matlab/run.ps1
@@ -1,1 +1,5 @@
-matlab -nodisplay -nosplash -nodesktop -r "run('./mstm.m'); exit;"
+$srcpath = $MyInvocation.MyCommand.Path
+$workdir = Split-Path -Path $srcpath
+$mpath = Join-Path -Path $workdir -ChildPath "mstm.m"
+
+matlab -nodisplay -nosplash -nodesktop -r "run('$mpath'); exit;"
