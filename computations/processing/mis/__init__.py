17a5077693a7e78d1a0afe54dc0607aa242827e7
Lev Litvinov <misery7100@gmail.com>
2022-04-25 09:40:44 +0300

    Minor edits: - old line plots in base_model updated; - text updates; - hkl plots with default palette.

--- computations/processing/mis/__init__.py
+++ computations/processing/mis/__init__.py
@@ -6,3 +6,13 @@ from .matlab import MSTM as MstmEngine
 from .lpic import Engine as LpicEngine
 from .mesh import MeshGenerator
 from .eqs import resonance_m_squared
+
+default_palette = [
+        
+        '#4078c0', 
+        '#6cc644',
+        '#bd2c00',
+        '#c9510c',
+        '#6e5494'
+    
+    ]
