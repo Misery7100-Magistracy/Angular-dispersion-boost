ba87a9a691e03ebf3b967f06eed98626976b2cf8
Lev Litvinov <misery7100@gmail.com>
2022-05-03 15:34:07 +0300

    grid on 2D plots added, edits in text...

--- computations/processing/msevn/utils.py
+++ computations/processing/msevn/utils.py
@@ -9,9 +9,9 @@ from .conf.load import plot_config as config
 def configure_mpl(config: dict = config) -> None:
 
     plt.rc('text', usetex=True)
-    plt.rcParams['contour.negative_linestyle'] = 'solid'
     mpl.rcParams.update(config.get('texrc'))
     plt.rc('text.latex', preamble=r'\usepackage{fouriernc}')
     sns.set(font_scale=float(config.get('font_scale')), style='white')
+    mpl.rcParams.update(config.get('postrc'))
 
 # ------------------------- #
